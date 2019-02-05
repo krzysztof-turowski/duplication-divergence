809e49a370a2bc274c19a77a1fdafed29cdbe519
Krzysztof Turowski <kturowsk@purdue.edu>
2019-02-05 17:38:10 -0500

    Add LaTeX figure style
    Note: text.usetex option requires LaTeX installed

--- dd_infer_parameters_plot.py
+++ dd_infer_parameters_plot.py
@@ -1,5 +1,5 @@
 # Tool for plotting the results from dd_infer_parameters.
-# Run: ./dd_infer_parameters FILE [--pdf]
+# Run: ./dd_infer_parameters FILE [--export {pdf}]
 
 import argparse
 import matplotlib
@@ -8,8 +8,35 @@ import numpy
 import os
 import sys
 
+TEXT_WIDTH_PT = 506.295 # Get this from LaTeX using \the\textwidth
+FIGURE_SIZE_SCALE = 0.5
+
 PASTOR_SATORRAS_PATTERN = [False, True, False]
 CHUNG_LU_PATTERN = [False, False, True]
+
+def initialize_figure():
+  publication_with_latex = {
+    "pgf.texsystem": "pdflatex", # change this if using xetex or lautex
+    # "text.usetex": True, # use LaTeX to write all text
+    "font.family": "serif",
+    "font.serif": [], # blank entries should cause plots to inherit fonts from the document
+    "font.sans-serif": [],
+    "font.monospace": [],
+    "axes.labelsize": 8, # LaTeX default is 10pt font.
+    "font.size": 8,
+    "legend.fontsize": 8, # Make the legend/label fonts a little smaller
+  }
+  def figure_size(): 
+    inches_per_pt = 1.0 / 72.27 # Convert pt to inch
+    golden_mean = (numpy.sqrt(5.0) - 1.0) / 2.0 # Aesthetic ratio (you could change this)
+    figure_width = TEXT_WIDTH_PT * inches_per_pt * FIGURE_SIZE_SCALE
+    figure_height = figure_width * golden_mean
+    return [figure_width, figure_height]
+  pyplot.style.use('ggplot')
+  matplotlib.rcParams.update(publication_with_latex)
+  matplotlib.rcParams['savefig.dpi'] = 125
+  matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath,amssymb,amsfonts}"]
+  matplotlib.rcParams['figure.figsize'] = figure_size()
 
 def read_data(data):
   values = numpy.array([[[float(value) if value != '' else None for value in interval.split(',')]
@@ -37,18 +64,16 @@ def plot_parameter(X, Y_min, Y, Y_max, label, color):
 def plot_data(file, filename, export):
   name = os.path.splitext(filename.strip())[0]
   data = file.readlines()
+  initialize_figure()
   plot_parameter(*read_data(data[0]), label = 'Degree', color = 'red')
   plot_parameter(*read_data(data[1]), label = 'Degree squared', color = 'magenta')
   plot_parameter(*read_data(data[2]), label = 'Open triangles', color = 'blue')
   plot_parameter(*read_data(data[3]), label = 'Triangles', color = 'green')
   pyplot.legend(loc = 'upper left')
   if export == 'pdf':
-    pyplot.savefig('temp/{0}.pdf'.format(filename), bbox_inches = 'tight', pad_inches = 0.05)
+    pyplot.savefig('temp/{0}.pdf'.format(name), bbox_inches = 'tight', pad_inches = 0.05)
   else:
     pyplot.show()
-
-matplotlib.rcParams['savefig.dpi'] = 125
-pyplot.style.use('ggplot')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', metavar = 'FILE', help = 'data to plot')
