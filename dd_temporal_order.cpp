1e1cceafd9923d0a2e41a44875c57a12ce71524c
Krzysztof Turowski <kturowsk@purdue.edu>
2019-02-16 23:08:51 -0500

    Compute p_uv coefficients

--- dd_temporal_order.cpp
+++ dd_temporal_order.cpp
@@ -21,7 +21,7 @@ vector<int> generate_permutation(const int &n, const int &n0) {
     S[i] = i;
   }
   for (int i = n - 1; i > n0; i--) {
-    std::uniform_int_distribution<int> swap_distribution(n0, i);
+    uniform_int_distribution<int> swap_distribution(n0, i);
     int index = swap_distribution(generator);
     swap(S[i], S[index]);
   }
@@ -87,6 +87,11 @@ map<mpz_class, double> get_permutation_probabilities(
   if (G.getVertNo() == n0) {
     mpz_class sigma = encode_permutation(S);
     permutations.insert(make_pair(sigma, p));
+    // cout << "PERMUTATION: ";
+    // for (int i = 0; i < static_cast<int>(S.size()); i++) {
+      // cout << S[i] << " ";
+    // }
+    // cout << p << endl;
     return permutations;
   }
 
@@ -115,27 +120,45 @@ map<mpz_class, double> get_permutation_probabilities(
   return permutations;
 }
 
-map<mpz_class, double> get_permutation_probabilities(Graph &G, const int &n0, const Parameters &params) {
-  NeighborhoodStructure aux(G);
-  vector<int> S(G.getVertNo(), -1);
+map<mpz_class, double> get_permutation_probabilities(const Graph &G, const int &n0, const Parameters &params) {
+  Graph H(G);
+  NeighborhoodStructure aux(H);
+  vector<int> S(H.getVertNo(), -1);
   for (int i = 0; i < n0; i++) {
     S[i] = i;
   }
-  return get_permutation_probabilities(G, n0, params, aux, S, 1.0);
+  map<mpz_class, double> permutations = get_permutation_probabilities(H, n0, params, aux, S, 1.0);
+
+  double total_probability = accumulate(
+    permutations.begin(), permutations.end(), 0.0,
+    [] (double value, const map<mpz_class, double>::value_type &permutation) { return value + permutation.second; });
+  for (auto &permutation : permutations) {
+    permutation.second /= total_probability;
+  }
+  return permutations;
 }
 
 void LP_bound_exact(const int &n, const int &n0, const Parameters &params) {
   Graph G = generate_seed_koala(n0, 1.0);
   generate_graph_koala(G, n, params);
 
-  Graph H(G);
   vector<int> S = generate_permutation(n, n0);
-  apply_permutation(H, S);
+  apply_permutation(G, S);
 
   // compute P(Pi_n = sigma_n | Pi_n(G_n) = h_n, G_n0 = g_n0)
-  map<mpz_class, double> permutations = get_permutation_probabilities(H, n0, params);
+  map<mpz_class, double> permutations = get_permutation_probabilities(G, n0, params);
 
   // compute p_uv coefficients
+  map<pair<int, int>, double> p_uv;
+  for (auto &permutation : permutations) {
+    vector<int> S_found = decode_permutation(permutation.first, n);
+    for (int i = n0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+          p_uv[make_pair(S_found[i], S_found[j])] += permutation.second;
+      }
+    }
+  }
+  
   // construct and solve LP
   // export to file and to stdout
 }
@@ -150,7 +173,7 @@ int main(int argc, char *argv[]) {
       LP_bound_exact(n, n0, params);
     }
     else {
-      throw std::invalid_argument("Invalid action: " + action);
+      throw invalid_argument("Invalid action: " + action);
     }
   } catch (const exception &e) {
     cerr << "ERROR: " << e.what() << endl;
