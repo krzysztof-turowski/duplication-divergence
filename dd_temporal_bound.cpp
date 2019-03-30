7b3dcf0242dcce035da0cc57acf9c3bfe5abe3e2
Krzysztof Turowski <kturowsk@purdue.edu>
2019-03-30 02:09:35 -0400

    Parametrize dd_temporal_bound.cpp

--- dd_temporal_bound.cpp
+++ dd_temporal_bound.cpp
@@ -4,6 +4,7 @@
 
 // TODO(kturowski): deal gurobi output suppression and output to cout instead of cerr
 
+#include "./dd_input.h"
 #include "./dd_temporal.h"
 
 #if defined(glpk)
@@ -16,7 +17,7 @@
 
 using namespace std;
 
-const int G_TRIES = 1, SIGMA_TRIES = 10000;
+int G_TRIES, SIGMA_TRIES;
 const double EPS_MIN = 0.05, EPS_STEP = 0.05;
 const long double RANDOM_WALK_THRESHOLD = 1.0;
 
@@ -75,7 +76,7 @@ vector<double> LP_bound_random_walk_single(
   std::uniform_real_distribution<double> pick_distribution(0.0, 1.0);
   std::map<mpz_class, long double> permutations;
   auto sample_permutations = get_log_permutation_probabilities_sampling(
-      G, get_graph_size(G0), params, SamplingMethod::MIN_DISCARD, SIGMA_TRIES);
+      G, get_graph_size(G0), params, SamplingMethod::UNIFORM, SIGMA_TRIES);
   std::priority_queue<std::pair<long double, mpz_class>> Q;
   for (const auto &permutation : sample_permutations) {
     Q.push(std::make_pair(permutation.second, permutation.first));
@@ -142,8 +143,8 @@ vector<double> LP_bound_approximate_single(
 }
 
 void LP_bound_exact(
-    const int &n, const int &n0, const Parameters &params, ostream &out_file) {
-  Graph G0 = generate_seed(n0, 0.6);
+    const int &n, const int &n0, const Parameters &params, const double &p0, ostream &out_file) {
+  Graph G0 = generate_seed(n0, p0);
   vector<double> epsilon;
   for (double eps = EPS_MIN; eps <= 1.0 + 10e-9; eps += EPS_STEP) {
     epsilon.push_back(eps);
@@ -170,8 +171,8 @@ void LP_bound_exact(
 }
 
 void LP_bound_random_walk(
-    const int &n, const int &n0, const Parameters &params, ostream &out_file) {
-  Graph G0 = generate_seed(n0, 0.6);
+    const int &n, const int &n0, const Parameters &params, const double &p0, ostream &out_file) {
+  Graph G0 = generate_seed(n0, p0);
   vector<double> epsilon;
   for (double eps = EPS_MIN; eps <= 1.0 + 10e-9; eps += EPS_STEP) {
     epsilon.push_back(eps);
@@ -200,9 +201,9 @@ void LP_bound_random_walk(
 }
 
 void LP_bound_approximate(
-    const int &n, const int &n0, const Parameters &params, const SamplingMethod &algorithm,
-    ostream &out_file) {
-  Graph G0 = generate_seed(n0, 0.6);
+    const int &n, const int &n0, const Parameters &params, const double &p0,
+    const SamplingMethod &algorithm, ostream &out_file) {
+  Graph G0 = generate_seed(n0, p0);
   vector<double> epsilon;
   for (double eps = EPS_MIN; eps <= 1.0 + 10e-9; eps += EPS_STEP) {
     epsilon.push_back(eps);
@@ -232,29 +233,35 @@ void LP_bound_approximate(
       epsilon, solution, n, n0, params, out_file);
 }
 
-int main(int, char *argv[]) {
+int main(int argc, char **argv) {
   try {
-    string action(argv[1]), mode(argv[2]);
-    int n = stoi(argv[3]), n0 = stoi(argv[4]);
-    Parameters params;
-    params.initialize(mode, argv + 5);
+    TEnv environment = prepare_environment(argc, argv);
+    G_TRIES = read_int(environment, "-gt:", 100, "G_TRIES");
+    SIGMA_TRIES = read_int(environment, "-st:", 100000, "SIGMA_TRIES");
+
+    string algorithm = read_string(
+        environment, "-algorithm:", "all", "Sampling algorithm to run");
+    string mode = read_mode(environment);
+    const int n = read_n(environment), n0 = read_n0(environment);
+    const double p0 = read_p0(environment);
+    Parameters params = read_parameters(environment);
     string name(TEMP_FOLDER + get_synthetic_filename(n, n0, params, "TC"));
-    if (action == "exact") {
+    if (algorithm == "exact") {
       if (!validate_problem_size(n, n0)) {
         throw out_of_range(
             "Graph too large for exact mode: n = " + to_string(n) + ", n0 = " + to_string(n0));
       }
       ofstream out_file(name, ios_base::app);
-      LP_bound_exact(n, n0, params, out_file);
-    } else if (action == "random_walk") {
+      LP_bound_exact(n, n0, params, p0, out_file);
+    } else if (algorithm == "random_walk") {
       ofstream out_file(name, ios_base::app);
-      LP_bound_random_walk(n, n0, params, out_file);
-    } else if (SAMPLING_METHOD_REVERSE_NAME.count(action)) {
+      LP_bound_random_walk(n, n0, params, p0, out_file);
+    } else if (SAMPLING_METHOD_REVERSE_NAME.count(algorithm)) {
       ofstream out_file(name, ios_base::app);
       LP_bound_approximate(
-          n, n0, params, SAMPLING_METHOD_REVERSE_NAME.find(action)->second, out_file);
+          n, n0, params, p0, SAMPLING_METHOD_REVERSE_NAME.find(algorithm)->second, out_file);
     } else {
-      throw invalid_argument("Invalid action: " + action);
+      throw invalid_argument("Invalid algorithm: " + algorithm);
     }
   } catch (const exception &e) {
     cerr << "ERROR: " << e.what() << endl;
