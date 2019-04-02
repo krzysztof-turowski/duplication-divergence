98c8927fd6b72caa38f6c2683a3a9fdbe565a8f0
Krzysztof Turowski <kturowsk@purdue.edu>
2019-04-02 02:15:31 -0400

    Remove random walk code

--- dd_temporal_bound.cpp
+++ dd_temporal_bound.cpp
@@ -49,63 +49,7 @@ vector<double> LP_bound_exact_single(
 
   auto permutations = get_log_permutation_probabilities(G, get_graph_size(G0), params);
   normalize_log_probabilities(permutations);
-  print_best_permutations(
-      permutations, G, params, get_graph_size(G0), "exact", PERMUTATION_COUNT_LIMIT);
   auto p_uv = get_p_uv_from_permutations(permutations, n, get_graph_size(G0));
-  set_perfect_pairs(p_uv, perfect_pairs);
-  vector<double> solutions;
-  for (const double &eps : epsilon) {
-    double solution;
-    tie(solution, ignore) = LP_solve(p_uv, n, get_graph_size(G0), eps);
-    solutions.push_back(solution);
-  }
-  return solutions;
-}
-
-vector<double> LP_bound_random_walk_single(
-    const Graph &G0, const int &n, const Parameters &params, const vector<double> &epsilon,
-    const set<VertexPair> &perfect_pairs) {
-  const int SIGMA_RANDOM_WALK_LENGTH = 10000, SIGMA_BEST = 100;
-  Graph G(G0);
-  generate_graph(G, n, params);
-
-  int n0 = get_graph_size(G0);
-
-  std::random_device device;
-  std::mt19937 generator(device());
-  std::uniform_real_distribution<double> pick_distribution(0.0, 1.0);
-  std::map<mpz_class, long double> permutations;
-  auto sample_permutations = get_log_permutation_probabilities_sampling(
-      G, get_graph_size(G0), params, get_DAG_from_perfect_pairs(perfect_pairs, n),
-      SamplingMethod::UNIFORM, SIGMA_TRIES);
-  std::priority_queue<std::pair<long double, mpz_class>> Q;
-  for (const auto &permutation : sample_permutations) {
-    Q.push(std::make_pair(permutation.second, permutation.first));
-  }
-  for (int i = 0; i < SIGMA_BEST; i++) {
-    vector<int> S = decode_permutation(Q.top().second, n);
-    long double score = get_log_permutation_probability(G, n0, params, reverse_permutation(S));
-    mpz_class best_sigma = Q.top().second;
-    long double best_score = score;
-    for (int j = 0; j < SIGMA_RANDOM_WALK_LENGTH; j++) {
-      std::uniform_int_distribution<int> swap_distribution(n0, n - 1);
-      int u = swap_distribution(generator), v = swap_distribution(generator);
-      if (u == v) {
-        continue;
-      }
-      swap(S[u], S[v]);
-      score = get_log_permutation_probability(G, n0, params, reverse_permutation(S));
-      if (best_score < score) {
-        best_score = score, best_sigma = encode_permutation(S);
-      } else {
-        swap(S[u], S[v]);
-      }
-    }
-    Q.pop();
-  }
-  normalize_log_probabilities(permutations);
-  print_best_permutations(permutations, G, params, n0, "random_walk", PERMUTATION_COUNT_LIMIT);
-  auto p_uv = get_p_uv_from_permutations(permutations, n, n0);
   set_perfect_pairs(p_uv, perfect_pairs);
   vector<double> solutions;
   for (const double &eps : epsilon) {
@@ -130,9 +74,6 @@ vector<double> LP_bound_approximate_single(
           G, get_graph_size(G0), params, get_DAG_from_perfect_pairs(perfect_pairs, n),
           algorithm, SIGMA_TRIES);
   normalize_log_probabilities(permutations);
-  print_best_permutations(
-      permutations, G, params, get_graph_size(G0),
-      SAMPLING_METHOD_NAME.find(algorithm)->second, PERMUTATION_COUNT_LIMIT);
   auto p_uv = get_p_uv_from_permutations(permutations, n, get_graph_size(G0));
   set_perfect_pairs(p_uv, perfect_pairs);
   vector<double> solutions;
@@ -172,36 +113,6 @@ void LP_bound_exact(
   print_density_precision("exact", epsilon, solution, n, n0, params, out_file);
 }
 
-void LP_bound_random_walk(
-    const int &n, const int &n0, const Parameters &params, const double &p0, ostream &out_file) {
-  Graph G0 = generate_seed(n0, p0);
-  vector<double> epsilon;
-  for (double eps = EPS_MIN; eps <= 1.0 + 10e-9; eps += EPS_STEP) {
-    epsilon.push_back(eps);
-  }
-  vector<double> solution(epsilon.size(), 0.0);
-  vector<vector<double>> solutions(G_TRIES);
-  #pragma omp parallel for
-  for (int i = 0; i < G_TRIES; i++) {
-    solutions[i] = LP_bound_random_walk_single(G0, n, params, epsilon, set<VertexPair>());
-    #pragma omp critical
-    {
-      cerr << "Finished run " << i + 1 << "/" << G_TRIES << endl;
-    }
-  }
-  for (int i = 0; i < G_TRIES; i++) {
-    transform(
-        solution.begin(), solution.end(), solutions[i].begin(), solution.begin(),
-        std::plus<double>());
-  }
-  for (auto &sol : solution) {
-    sol /= G_TRIES;
-  }
-  print_density_precision(
-      "random_walk-" + to_string(G_TRIES) + "-" + to_string(SIGMA_TRIES),
-      epsilon, solution, n, n0, params, out_file);
-}
-
 void LP_bound_approximate(
     const int &n, const int &n0, const Parameters &params, const double &p0,
     const SamplingMethod &algorithm, ostream &out_file) {
@@ -238,8 +149,8 @@ void LP_bound_approximate(
 int main(int argc, char **argv) {
   try {
     Env = prepare_environment(argc, argv);
-    G_TRIES = read_int(Env, "-gt:", 100, "G_TRIES");
-    SIGMA_TRIES = read_int(Env, "-st:", 100000, "SIGMA_TRIES");
+    G_TRIES = read_int(Env, "-gt:", 1, "G_TRIES");
+    SIGMA_TRIES = read_int(Env, "-st:", 1, "SIGMA_TRIES");
 
     string algorithm = read_string(Env, "-algorithm:", "all", "Sampling algorithm to run");
     const int n = read_n(Env), n0 = read_n0(Env);
@@ -253,9 +164,6 @@ int main(int argc, char **argv) {
       }
       ofstream out_file(name, ios_base::app);
       LP_bound_exact(n, n0, params, p0, out_file);
-    } else if (algorithm == "random_walk") {
-      ofstream out_file(name, ios_base::app);
-      LP_bound_random_walk(n, n0, params, p0, out_file);
     } else if (SAMPLING_METHOD_REVERSE_NAME.count(algorithm)) {
       ofstream out_file(name, ios_base::app);
       LP_bound_approximate(
