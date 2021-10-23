59f02c89563a7a642caa481ac3c91bdd593108c0
Marcin Serwin <marcin.serwin0@protonmail.com>
2021-10-23 13:08:47 +0200

    Implement BA model generation

--- src/dd_header.h
+++ src/dd_header.h
@@ -229,7 +229,37 @@ Graph generate_sticky_graph(const Parameters &params,
 }
 
 Graph generate_ba_graph(Graph &&G, const int &n, const Parameters &params) {
-  return Graph();
+  std::random_device device;
+  std::mt19937 generator(device());
+
+  const uint32_t &m_param = params.m;
+
+  const size_t seed_size = G.size();
+  if (m_param > seed_size) {
+    throw std::invalid_argument("m parameter must not be greater than seed graph size.");
+  }
+  G.resize(n);
+
+  std::vector<int> degrees(n, 0);
+  for (size_t i = 0; i < seed_size; i++) {
+    degrees[i] = G[i].size();
+  }
+
+  for (int i = seed_size; i < n; i++) {
+    std::discrete_distribution<int> distribution(degrees.begin(), degrees.end());
+    while (G[i].size() < m_param) {
+      const int new_edge = distribution(generator);
+      if (G[i].find(new_edge) != G[i].end()) {
+        continue;
+      }
+
+      degrees[i]++;
+      degrees[new_edge]++;
+      G[i].insert(new_edge);
+      G[new_edge].insert(i);
+    }
+  }
+  return std::move(G);
 }
 
 Graph generate_seed_simple(const int &n0, const double &p0) {
