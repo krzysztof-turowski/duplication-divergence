2d488f699083db9d119e78d6313affbd7f868e4d
Marcin Serwin <marcin.serwin0@protonmail.com>
2021-12-06 22:38:05 +0100

    Add degree distribution metric

---
+++ src/metrics/degree_distribution.cpp
@@ -0,0 +1,15 @@
+#include "degree_distribution.h"
+
+std::vector<size_t> get_degree_distribution(const Graph &G) {
+  std::vector<size_t> result(G.size(), 0);
+  size_t max_degree = 0;
+  for (auto &&v : G) {
+    max_degree = std::max(max_degree, v.size());
+    result[v.size()]++;
+  }
+
+  result.resize(max_degree + 1);
+  result.shrink_to_fit();
+
+  return result;
+}
---
+++ src/metrics/degree_distribution.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "common.h"
+
+std::vector<size_t> get_degree_distribution(const Graph &G);
