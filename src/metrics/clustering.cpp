dc50cfdb044d03efcba84dffb1697f2a7f73db18
Marcin Serwin <marcin.serwin0@protonmail.com>
2021-12-06 19:20:18 +0100

    Add clustering coefficient of order 4

--- src/metrics/clustering.cpp
+++ src/metrics/clustering.cpp
@@ -54,3 +54,39 @@ double clustering_coefficient_three(const Graph &graph) {
   auto four_cliques = count_four_cliques(graph);
   return static_cast<double>(3 * (3 + 1) * four_cliques.first) / four_cliques.second;
 }
+
+std::pair<int64_t, int64_t> count_five_cliques(const Graph &G) {
+  std::pair<int64_t, int64_t> result{ 0, 0 };
+
+  for (auto v : G) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+      for (auto jt = std::next(it, 1); jt != v.end(); ++jt) {
+        if (!G[*it].count(*jt)) {
+          continue;
+        }
+
+        for (auto kt = std::next(jt, 1); kt != v.end(); ++kt) {
+          if (!G[*it].count(*kt) || !G[*jt].count(*kt)) {
+            continue;
+          }
+          result.second += v.size() - 3;
+
+          for (auto lt = std::next(kt, 1); lt != v.end(); ++lt) {
+            if (G[*it].count(*lt) && G[*jt].count(*lt) && G[*kt].count(*lt)) {
+              result.first++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  result.first /= 5;
+
+  return result;
+}
+
+double clustering_coefficient_four(const Graph &graph) {
+  auto five_cliques = count_five_cliques(graph);
+  return static_cast<double>(5 * (5 + 1) * five_cliques.first) / five_cliques.second;
+}
--- src/metrics/clustering.h
+++ src/metrics/clustering.h
@@ -3,3 +3,5 @@
 #include "common.h"
 
 double clustering_coefficient_two(const Graph &);
+double clustering_coefficient_three(const Graph &graph);
+double clustering_coefficient_four(const Graph &graph);
